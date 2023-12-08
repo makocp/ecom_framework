4c5a289c8f609384c4663e92a52b89aec91a6906
Mako CP <mako.codeproducer@gmail.com>
2023-12-08 14:28:51 +0100

    minor changes

--- navigators/TabsNavigator.tsx
+++ navigators/TabsNavigator.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MainView from '../ui/MainView';
 import CartView from '../ui/CartView';
 import ProfileView from '../ui/ProfileView';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
 
 export type TabsStackParamList = {
     MainView: undefined;
@@ -11,21 +12,25 @@ export type TabsStackParamList = {
     ProfileView: undefined;
 }
 
-const navigationOptions = {
-    // tabBarShowLabel: false,
+// if assigned to the Navigator, the options are applied to all sub screens.
+const screenOptionsAll = {
+    tabBarShowLabel: false,
     tabBarHideOnKeyboard: true,
     headerShown: false,
 }
-
 
 const TabsStack = createBottomTabNavigator<TabsStackParamList>();
 
 const TabsNavigator = () => {
     return (
-        <TabsStack.Navigator>
-            <TabsStack.Screen name='MainView' component={MainView} options={navigationOptions} />
-            <TabsStack.Screen name='CartView' component={CartView} options={navigationOptions} />
-            <TabsStack.Screen name='ProfileView' component={ProfileView} options={navigationOptions} />
+        <TabsStack.Navigator screenOptions={screenOptionsAll}>
+            <TabsStack.Screen name='MainView' component={MainView} options={{
+                tabBarIcon(props) {
+                    return <Icon name='home'/>
+                },
+            }}/>
+            <TabsStack.Screen name='CartView' component={CartView} />
+            <TabsStack.Screen name='ProfileView' component={ProfileView} />
         </TabsStack.Navigator>
     )
 }
