234ee195224d98dcdb3e0ba66802431055b3d24c
Mako CP <mako.codeproducer@gmail.com>
2023-12-13 14:02:34 +0100

    minor changes

--- navigators/TabsNavigator.tsx
+++ navigators/TabsNavigator.tsx
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MainView from '../ui/MainView';
 import CartView from '../ui/CartView';
 import ProfileView from '../ui/ProfileView';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 export type TabsStackParamList = {
@@ -19,20 +19,45 @@ const screenOptionsAll = {
     headerShown: false,
 }
 
+
+// alternative: options can be extracted.
+// type TabIconProps = {
+//     focused: boolean
+//     color: string
+//     size: number
+// }
+
+// const tabOptions = (name: string) => ({
+//     tabBarLabel: name,
+//     tabBarIcon: ({ focused, color, size }: TabIconProps) => (
+//         <MaterialCommunityIcon
+//             name={focused ? name : name + "-outline"}
+//             color={color}
+//             size={size} />
+//     ),
+// });
+
 const TabsStack = createBottomTabNavigator<TabsStackParamList>();
 
 const TabsNavigator = () => {
     return (
         <TabsStack.Navigator screenOptions={screenOptionsAll}>
             <TabsStack.Screen name='MainView' component={MainView} options={{
-                tabBarLabel: 'Home',
-                tabBarIcon: ({color, size}) => (
-                    <Icon name='home' color={color} size={size}/>
-                ),
-                tabBarBadge: 3,
-            }}/>
-            <TabsStack.Screen name='CartView' component={CartView} />
-            <TabsStack.Screen name='ProfileView' component={ProfileView} />
+                tabBarIcon: ({ focused, color, size }) => {
+                    return <MaterialCommunityIcon name={focused ? 'shopping' : 'shopping-outline'} color={color} size={size} />
+                }
+            }} />
+            <TabsStack.Screen name='CartView' component={CartView} options={{
+                tabBarIcon: ({ focused, color, size }) => {
+                    return <MaterialCommunityIcon name={focused ? 'cart' : 'cart-outline'} color={color} size={size} />
+                },
+                // tabBarBadge: 3
+            }} />
+            <TabsStack.Screen name='ProfileView' component={ProfileView} options={{
+                tabBarIcon: ({ focused, color, size }) => {
+                    return <MaterialCommunityIcon name={focused ? 'account' : 'account-outline'} color={color} size={size} />
+                }
+            }} />
         </TabsStack.Navigator>
     )
 }
