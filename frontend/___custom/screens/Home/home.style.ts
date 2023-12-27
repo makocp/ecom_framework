a35db94424ff6fba88ce332299cd69178e9960f3
Mako CP <mako.codeproducer@gmail.com>
2023-12-27 21:08:18 +0100

    adding styles

---
+++ frontend/___custom/screens/Home/home.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontFamily: 'bold',
+        fontSize: 20
+    },
+    appBarWrapper: {
+        marginHorizontal: 22,
+        marginTop: 10
+    }
+});
+
+export default styles;
