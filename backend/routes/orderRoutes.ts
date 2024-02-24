e999bfe53f94a21818d45c2bf4533fda5abe4dce
Mako CP <mako.codeproducer@gmail.com>
2024-02-24 01:29:13 +0100

    backend endpoint for order creation done

--- backend/routes/orderRoutes.ts
+++ backend/routes/orderRoutes.ts
@@ -1,4 +1,35 @@
 import * as express from 'express';
+import {CartProduct} from "../types/types";
+import * as path from "path";
+import * as fs from "fs";
+
 const router = express.Router();
+
+router.post('/create-order', (req, res) => {
+    const cartProducts = req.body;
+    const orderId = Date.now().toString();
+
+    const order = mapCartProductsToOrderStructure(cartProducts, orderId);
+
+    const ordersDir = path.join(__dirname, '..', 'orders');
+    if (!fs.existsSync(ordersDir)) {
+        fs.mkdirSync(ordersDir);
+    }
+    fs.writeFileSync(path.join(ordersDir, `${orderId}.json`), JSON.stringify(order, null, 2));
+
+    res.send({orderId});
+});
+
+function mapCartProductsToOrderStructure(cartProducts: CartProduct[], orderId: string) {
+    return {
+        id: orderId,
+        userId: 12, // Example, you need to get this from somewhere
+        products: cartProducts, // Assuming CartProduct and Product are compatible
+        amountTotal: 89990,
+        shippingAddress: 'Some address', // Get this from the user
+        isPaid: true,
+        isDelivered: false,
+    };
+}
 
 module.exports = router;
--- frontend/src/routes/Routes.ts
+++ frontend/src/routes/Routes.ts
@@ -1,3 +1,4 @@
 export const API_URL = 'http://localhost:4242';
+export const API_URL_CREATE_ORDER = 'http://localhost:4242/orders/create-order';
 export const API_URL_PUB_STRIPE_KEY = 'http://localhost:4242/payments/stripe-key';
 export const API_URL_PAYMENT_INTENT = 'http://localhost:4242/payments/paymentIntent';
