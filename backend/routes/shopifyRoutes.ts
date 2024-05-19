36e5809da4684fe48ec3873c9cb1b6529a575ef9
Mako CP <mako.codeproducer@gmail.com>
2024-05-19 23:32:22 +0200

    add order backend endpoint

--- backend/routes/shopifyRoutes.ts
+++ backend/routes/shopifyRoutes.ts
@@ -33,7 +33,38 @@ router.get('/products', async (req, res) => {
         return res.status(200).send(body);
     } catch (error) {
         console.error(error);
-        res.status(500).json({error: 'Failed to get access scopes from Shopify'});
+        res.status(500).json({error: 'Failed to load products from Shopify'});
+    }
+});
+
+router.post('/order', async (req, res) => {
+    const order = req.body;
+
+    console.log('req.body', req.body);
+    console.log('order',order);
+
+    if (!order) {
+        return res.status(400).json({error: 'Order object is required'});
+    }
+
+    try {
+        const response = await client.post(`/admin/api/2024-04/orders.json`, {
+            data: {
+                order
+            }
+        });
+
+        if (!response.ok) {
+            const errorBody = await response.json();
+            console.error('Error response from Shopify:', errorBody);
+            return res.status(response.status).json({error: 'Failed to create order in Shopify', details: errorBody});
+        }
+
+        const body = await response.json();
+        return res.status(201).send(body);
+    } catch (error) {
+        console.error('Error creating order:', error);
+        res.status(500).json({error: 'Failed to create order in Shopify'});
     }
 });
 
