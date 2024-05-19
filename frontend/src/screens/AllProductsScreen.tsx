d36194ab74d1aa774fa4c81151b1ca43cc94e59f
Mako CP <mako.codeproducer@gmail.com>
2024-05-19 23:52:19 +0200

    fix search query

--- frontend/src/screens/AllProductsScreen.tsx
+++ frontend/src/screens/AllProductsScreen.tsx
@@ -4,17 +4,15 @@ import {useSafeAreaInsets} from "react-native-safe-area-context";
 import SearchBar from "../components/search/SearchBar";
 import {RouteProp, useRoute} from "@react-navigation/native";
 import {TabsStackParamList} from "../navigators/TabsNavigator";
-import {mockProducts} from "../data/mockData";
 import ProductCard from "../components/home/ProductCard";
 import AppBar from "../components/home/AppBar";
 import FadeInScreen from "./FadeInScreen";
 import useCleanToastsOnUnfocus from "../hooks/useCleanToastsOnUnfocus";
-import {IProduct} from "../types/types";
-import {useShopifyData} from "../providers/ProductData/ShopifyProvider";
+import {IShopifyProduct, useShopifyData} from "../providers/ProductData/ShopifyProvider";
 
 const AllProductsScreen = () => {
     const [searchQuery, setSearchQuery] = useState<string>('');
-    // const [filteredProducts, setFilteredProducts] = useState<IProduct[]>(mockProducts);
+    const [filteredProducts, setFilteredProducts] = useState<IShopifyProduct[]>([]);
     const insets = useSafeAreaInsets();
     const route = useRoute<RouteProp<TabsStackParamList, 'AllProductsScreen'>>();
     const searchBarRef = useRef<TextInput>(null);
@@ -22,53 +20,34 @@ const AllProductsScreen = () => {
 
     const {products} = useShopifyData();
 
-    // function debounce(func: any, wait: number) {
-    //     let timeout: any;
-    //
-    //     return function (...args: any[]) {
-    //         const later = () => {
-    //             clearTimeout(timeout);
-    //             func(...args);
-    //         };
-    //
-    //         clearTimeout(timeout);
-    //         timeout = setTimeout(later, wait);
-    //     };
-    // }
+    useEffect(() => {
+        if (route.params?.isFromSearch) {
+            searchBarRef.current?.focus();
+        }
+    }, [route.params?.isFromSearch]);
 
-    // const debouncedSetSearchQuery = useRef(debounce(setSearchQuery, 200)).current;
-
-    // useEffect(() => {
-    //     if (route.params?.isFromSearch) {
-    //         searchBarRef.current?.focus();
-    //     }
-    // }, [route.params?.isFromSearch]);
-
-    // useEffect(() => {
-    //     const lowercaseQuery = searchQuery.toLowerCase();
-    //     const filteredProducts = mockProducts.filter(product =>
-    //         product.title.toLowerCase().includes(lowercaseQuery) ||
-    //         product.category.toLowerCase().includes(lowercaseQuery) ||
-    //         product.description.toLowerCase().includes(lowercaseQuery)
-    //     );
-    //     setFilteredProducts(filteredProducts);
-    // }, [searchQuery]);
-
+    useEffect(() => {
+        const lowercaseQuery = searchQuery.toLowerCase();
+        const filteredProducts = products.filter(product =>
+            product.title.toLowerCase().includes(lowercaseQuery) ||
+            product.category?.toLowerCase().includes(lowercaseQuery) ||
+            product.description.toLowerCase().includes(lowercaseQuery)
+        );
+        setFilteredProducts(filteredProducts);
+    }, [searchQuery, products]);
 
     return (
         <FadeInScreen>
             <View style={[styles.container, {paddingTop: insets.top}]}>
-                <AppBar screenName={'All Products'}></AppBar>
+                <AppBar screenName={'All Products'} />
                 <SearchBar
                     ref={searchBarRef}
-                    // onChangeText={(text: string) => debouncedSetSearchQuery(text)}
-                    onChangeText={() => {
-                        console.log('placeholder')}}
+                    onChangeText={(text: string) => setSearchQuery(text)}
                 />
                 <View style={styles.scrollContainer}>
                     <FlatList
-                        data={products}
-                        renderItem={({item}) => <ProductCard product={item}/>}
+                        data={filteredProducts}
+                        renderItem={({item}) => <ProductCard product={item} />}
                         numColumns={2}
                         showsVerticalScrollIndicator={false}
                         alwaysBounceVertical={false}
